5ebb9aa2119f0c37c08332a3a00d84ad64b22f31
Diego Carrillo <61395884+DiegoCarrillo32@users.noreply.github.com>
2025-08-03 16:06:18 -0600

    added more stuff in the footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,13 +3,40 @@ import styles from "./index.module.scss";
 
 export const Footer = () => {
   const { t } = useTranslation("translation", { keyPrefix: "footer" });
+  const { t: info } = useTranslation("translation", {
+    keyPrefix: "info",
+  });
+  const phone = import.meta.env.VITE_WHATSAPP_PHONE || "";
+  const message = encodeURIComponent(info("wa-text"));
+
+  const handleClick = () => {
+    window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
+  };
   return (
     <footer className={styles.footer}>
-      {/* <img alt="Arenal Volcanic Villas logo" /> */}
-      <div>
-        <p className={styles.footer__text}>{t("connect")}</p>
-        <p className={styles.footer__subtext}>{t("instagram")}</p>
-        <p className={styles.footer__subtext}>{t("whatsapp")}</p>
+      <div className={styles.footer__wrapper}>
+        <div className={styles["footer__logo-wrapper"]}>
+          <img
+            className={styles["footer__logo"]}
+            alt="Arenal Volcanic Villas logo"
+          />
+        </div>
+        <div className={styles.footer__content}>
+          <div>
+            <p className={styles.footer__text}>{t("connect")}</p>
+            <p onClick={handleClick} className={styles.footer__subtext}>
+              {t("instagram")}
+            </p>
+            <p className={styles.footer__subtext}>{t("whatsapp")}</p>
+            <p className={styles.footer__subtext}>{t("facebook")}</p>
+          </div>
+
+          <div>
+            <p className={styles.footer__text}>{t("policy")}</p>
+            <p className={styles.footer__subtext}>{t("return-policy")}</p>
+            <p className={styles.footer__subtext}>{t("terms-conditions")}</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
