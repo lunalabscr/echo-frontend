24032ab5136638c430d78144483995b23eda6619
Diego Carrillo <61395884+DiegoCarrillo32@users.noreply.github.com>
2025-08-03 16:13:02 -0600

    updated sitemap and index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,10 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { LocalizedApp } from "./LocalizedApp";
 
 export const Index = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to={"/en"} />} />
+      <Route path="/" element={<LocalizedApp />} />
       <Route path="/:lng/*" element={<LocalizedApp />} />
     </Routes>
   );
