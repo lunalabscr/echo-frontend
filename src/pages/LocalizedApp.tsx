ec0c19fd2d8f6b81afeeaf669185adcbed397145
Diego Carrillo <61395884+DiegoCarrillo32@users.noreply.github.com>
2025-08-03 14:52:57 -0600

    Update LocalizedApp.tsx

--- src/pages/LocalizedApp.tsx
+++ src/pages/LocalizedApp.tsx
@@ -43,6 +43,7 @@ export const LocalizedApp = () => {
                 path={`/${lng}`}
               />
               <main>
+                <ScrollToTop />
                 <Hero />
                 <WhatWeOffer />
                 <Faq />
