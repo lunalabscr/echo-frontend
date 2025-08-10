5b203b8590ef17127d60b0ceb1ac2c848703e116
Diego Carrillo <61395884+DiegoCarrillo32@users.noreply.github.com>
2025-08-10 12:07:27 -0600

    style fixes

--- src/components/faq/index.tsx
+++ src/components/faq/index.tsx
@@ -6,26 +6,16 @@ import {
 } from "@radix-ui/react-accordion";
 import styles from "./index.module.scss";
 import { useTranslation } from "react-i18next";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 export const Faq = () => {
   const { t } = useTranslation("translation", { keyPrefix: "faq" });
   const accordionFaq = [
-    {
-      trigger: t("question1"),
-      content: t("answer1"),
-      value: "trigger-1",
-    },
-    {
-      trigger: t("question2"),
-      content: t("answer2"),
-      value: "trigger-2",
-    },
-    {
-      trigger: t("question3"),
-      content: t("answer3"),
-      value: "trigger-3",
-    },
+    { trigger: t("question1"), content: t("answer1"), value: "trigger-1" },
+    { trigger: t("question2"), content: t("answer2"), value: "trigger-2" },
+    { trigger: t("question3"), content: t("answer3"), value: "trigger-3" },
   ];
+
   return (
     <Accordion type="multiple" className={styles.accordion}>
       {accordionFaq.map((faq) => (
@@ -36,6 +26,11 @@ export const Faq = () => {
         >
           <AccordionTrigger className={styles.accordion__trigger}>
             {faq.trigger}
+            {/* Use the data-state attribute to swap icons */}
+            <span className={styles.iconWrapper}>
+              <ChevronDown className={styles.iconClosed} />
+              <ChevronUp className={styles.iconOpen} />
+            </span>
           </AccordionTrigger>
           <AccordionContent className={styles.accordion__content}>
             {faq.content}
